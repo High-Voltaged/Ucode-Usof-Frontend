bc13fa6295c20da1a4ddbf9b56346434357434b6
Polina Balazhy <lefugdot@gmail.com>
2022-10-30 00:57:47 +0300

    add search bar handling

---
+++ web/src/components/Dropdown/SearchDropdown.js
@@ -0,0 +1,80 @@
+import { Card, Grid, Text } from "@nextui-org/react";
+import { useNavigate } from "react-router-dom";
+import { postNav } from "~/consts/routes";
+import { colors } from "~/theme/config";
+import ErrorTitle from "../ErrorTitle/ErrorTitle";
+import Loader from "../Loader/Loader";
+
+const Wrapper = ({ children }) => (
+  <Grid.Container css={{ position: "absolute", top: "100%" }}>
+    <Card css={{ p: 10 }}>{children}</Card>
+  </Grid.Container>
+);
+
+const SearchDropdown = ({
+  search,
+  posts,
+  isFetching,
+  error,
+  setDropdown,
+  resetSearch,
+}) => {
+  const navigate = useNavigate();
+
+  const clickHandler = (id) => {
+    navigate(postNav.post(id));
+    setDropdown(false);
+    resetSearch();
+  };
+
+  if (!search) {
+    return <div></div>;
+  }
+
+  if (error) {
+    return (
+      <Wrapper>
+        <ErrorTitle size={24} text={error.data.message} />;
+      </Wrapper>
+    );
+  }
+
+  if (isFetching || !posts) {
+    return (
+      <Wrapper>
+        <Loader />
+      </Wrapper>
+    );
+  }
+
+  const postCards = posts
+    .filter((p) => p.title.toLowerCase().includes(search.toLowerCase()))
+    .slice(0, 5)
+    .map((p) => (
+      <Grid xs={12} css={{ p: 5 }}>
+        <Card
+          css={{ p: 10 }}
+          variant="bordered"
+          isPressable
+          isHoverable
+          onPress={() => clickHandler(p.id)}
+        >
+          <Text color={colors.feature} h5>
+            {p.title}
+          </Text>
+        </Card>
+      </Grid>
+    ));
+
+  if (!postCards.length) {
+    return (
+      <Wrapper>
+        <ErrorTitle size={24} text="No posts found." />
+      </Wrapper>
+    );
+  }
+
+  return <Wrapper>{postCards}</Wrapper>;
+};
+
+export default SearchDropdown;
--- web/src/components/SearchInput/SearchInput.js
+++ web/src/components/SearchInput/SearchInput.js
@@ -1,18 +1,61 @@
+import { Button, Grid } from "@nextui-org/react";
+import { useFormik } from "formik";
+import { useState } from "react";
 import { FaSearch } from "react-icons/fa";
 import InputField from "~/components/InputField/InputField";
+import { useLazyGetPostsQuery } from "~/redux/api/posts-api";
+import { colors } from "~/theme/config";
+import SearchDropdown from "../Dropdown/SearchDropdown";
 
 const SearchInput = () => {
+  const [dropdown, setDropdown] = useState(false);
+  const [getPosts, { data, isFetching, error }] = useLazyGetPostsQuery();
+
+  const { values, handleSubmit, handleBlur, setFieldValue } = useFormik({
+    initialValues: { search: "" },
+    onSubmit: async () => {
+      await getPosts();
+      setDropdown(true);
+    },
+  });
+
   return (
-    <InputField
-      placeholder="Search..."
-      aria-label="search"
-      contentLeft={<FaSearch size={16} />}
-      isLast
-      clearable
-      css={{
-        mw: "200px",
-      }}
-    ></InputField>
+    <Grid.Container css={{ position: "relative", ai: "center" }} gap={1}>
+      <Grid xs={7} sm={8}>
+        <InputField
+          name="search"
+          placeholder="Search..."
+          aria-label="search"
+          isLast
+          clearable
+          css={{ mw: 200 }}
+          setFieldValue={setFieldValue}
+          onBlur={handleBlur}
+          value={values.search}
+        ></InputField>
+      </Grid>
+      <Grid xs={3} sm={2} css={{ flexShrink: 0 }}>
+        <Button
+          icon={<FaSearch size={12} />}
+          onPress={handleSubmit}
+          auto
+          color={colors.feature}
+          size="sm"
+        />
+      </Grid>
+      {dropdown && (
+        <Grid xs={12}>
+          <SearchDropdown
+            search={values.search}
+            error={error}
+            isFetching={isFetching}
+            posts={data.posts}
+            setDropdown={setDropdown}
+            resetSearch={() => setFieldValue("search", "")}
+          />
+        </Grid>
+      )}
+    </Grid.Container>
   );
 };
 
